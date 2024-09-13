b7a55b6d7d35fdf89b26c559ec7b0170693c9dbe
DeathBladeGamerZ <166324810+DeathBladeGamerZ@users.noreply.github.com>
2024-09-13 15:12:23 +0530

    mobile view

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-let t = document.getElementById("topbtn");
-function scrollFunction()
-{
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-    {
-        t.style.display= "block";
-    }
-    else
-    {
-        t.style.display= "none";
-    }
-}
-function topfunction() {
-    document.documentElement.scrollTop = 0;
-}
 function load() 
 {
    window.open("https://www.youtube.com/@deathbladegamerz51907");
